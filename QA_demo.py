7c943d5eda31d21f5aebe6af8b3e637fbeb7a135
youseef <youseefmoemen293@gmail.com>
2023-06-30 15:58:50 +0300

    updates

--- QA_demo.py
+++ QA_demo.py
@@ -1,13 +1,14 @@
 import openai
 
-openai.api_key = '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'
+openai.api_key = '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'
 
 caption = ""
 with open('tested_captions/pizza_caption_v2', mode='r') as f:
     for line in f:
         caption += ' ' + line
 
-question = 'What is the ingredients in for the cooked pizza ?'
+# question = 'What is the chef is coking ?'
+question = 'What is the ingredients used in the pizza ?'
 prompt = f"the following text is video caption answer the following question:  {question} using the provided " \
          f"caption: {caption}"
 
@@ -20,5 +21,5 @@ chat = openai.ChatCompletion.create(
     model="gpt-3.5-turbo", messages=messages
 )
 reply = chat.choices[0].message.content
-
-print(reply)
+print(f'Question: {question}')
+print(f'Answer: {reply}')
